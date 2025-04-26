a2e35bd66605307c0d9dd221a9d329976bb982fd
Vignan Kamarthi <vignankamarthi@gmail.com>
2025-04-26 17:53:56 -0400

    tweaked initalization fatures.

--- src/facial_recognition_software/__init__.py
+++ src/facial_recognition_software/__init__.py
@@ -4,29 +4,21 @@
 # Define __all__ to control what's imported with "from facial_recognition_software import *"
 __all__ = ['FaceDetector', 'FaceMatcher', 'FaceAnonymizer', 'BiasAnalyzer']
 
-# Lazy import implementation
-class LazyLoader(object):
-    def __init__(self, local_name, parent_module_globals, name):
-        self.local_name = local_name
-        self.parent_module_globals = parent_module_globals
-        self.name = name
+# Create proper class-like lazy loaders that return the actual class when called
+class LazyClass:
+    def __init__(self, module_path, class_name):
+        self.module_path = module_path
+        self.class_name = class_name
+        self._class = None
         
-    def __getattr__(self, item):
-        module = self._load()
-        return getattr(module, item)
-        
-    def _load(self):
-        # Import the module and insert it into the parent's globals
-        module = __import__(self.name, fromlist=[None])
-        self.parent_module_globals[self.local_name] = module
-        return module
+    def __call__(self, *args, **kwargs):
+        if self._class is None:
+            module = __import__(self.module_path, fromlist=[self.class_name])
+            self._class = getattr(module, self.class_name)
+        return self._class(*args, **kwargs)
 
-# Set up lazy-loaded modules
-import sys as _sys
-_current_module = _sys.modules[__name__]
-
-# These will be imported only when actually needed
-FaceDetector = LazyLoader('FaceDetector', globals(), 'facial_recognition_software.face_detection')
-FaceMatcher = LazyLoader('FaceMatcher', globals(), 'facial_recognition_software.face_matching')
-FaceAnonymizer = LazyLoader('FaceAnonymizer', globals(), 'facial_recognition_software.anonymization')
-BiasAnalyzer = LazyLoader('BiasAnalyzer', globals(), 'facial_recognition_software.bias_testing')
+# Implement our classes with proper lazy loading
+FaceDetector = LazyClass('facial_recognition_software.face_detection', 'FaceDetector')
+FaceMatcher = LazyClass('facial_recognition_software.face_matching', 'FaceMatcher')
+FaceAnonymizer = LazyClass('facial_recognition_software.anonymization', 'FaceAnonymizer')
+BiasAnalyzer = LazyClass('facial_recognition_software.bias_testing', 'BiasAnalyzer')
